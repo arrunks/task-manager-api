6c37abfd497219697e2f7282310f2c05ee0703a5
Arun Kumar <arunk.s@outlook.com>
2024-11-21 19:38:24 +0530

    fixing eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,33 +1,25 @@
- // Code generated via "Slingshot" 
 // eslint.config.js
-import { Linter } from 'eslint';
-
-export default new Linter.Config({
-  // Specify the environment
+module.exports = {
   env: {
     browser: true,
     es2021: true,
     node: true,
   },
-  // Extend recommended configurations
   extends: [
     'eslint:recommended',
     'plugin:react/recommended', // If you're using React
   ],
-  // Specify the parser options
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  // Define global variables
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
   },
-  // Add custom rules
   rules: {
     'no-console': 'warn',
     'no-unused-vars': 'warn',
     'semi': ['error', 'always'],
   },
-});
+};
